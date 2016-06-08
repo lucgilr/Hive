2a7b3ce9911e983be9782deaef950ea792df27c7
lucgilr <luciagilroman@gmail.com>
2016-06-08 20:41:33 +0200

    [UI] Rules images in english + New Main Menu
    Just for big screens…

--- app/src/main/java/lucgilr/inf/uva/hivev2/UI/HowToPlay.java
+++ app/src/main/java/lucgilr/inf/uva/hivev2/UI/HowToPlay.java
@@ -2,11 +2,10 @@ package lucgilr.inf.uva.hivev2.UI;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 import lucgilr.inf.uva.hivev2.R;
 
@@ -15,20 +14,60 @@ import lucgilr.inf.uva.hivev2.R;
  */
 public class HowToPlay extends AppCompatActivity {
 
+    private String displayLanguage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-        /*this.setContentView(R.layout.how_to_play);
+        this.setContentView(R.layout.how_to_play);
 
-        WebView myWebView = (WebView) this.findViewById(R.id.webView);
-        myWebView.setWebViewClient(new WebViewClient());
+        //Language
+        displayLanguage = Locale.getDefault().getDisplayLanguage();
 
-        //myWebView.loadUrl("file:///android_assets/HowToPlay.html");
-        myWebView.loadUrl("https://www.google.es");*/
+        ImageView image_1 = (ImageView) this.findViewById(R.id.image_1);
+        ImageView image_2 = (ImageView) this.findViewById(R.id.image_2);
+        ImageView image_3 = (ImageView) this.findViewById(R.id.image_3);
+        ImageView image_4 = (ImageView) this.findViewById(R.id.image_4);
+        ImageView image_5 = (ImageView) this.findViewById(R.id.image_5);
+        ImageView image_6 = (ImageView) this.findViewById(R.id.image_6);
+        ImageView image_7 = (ImageView) this.findViewById(R.id.image_7);
+        ImageView image_8 = (ImageView) this.findViewById(R.id.image_8);
+        ImageView image_9 = (ImageView) this.findViewById(R.id.image_9);
+        ImageView image_10 = (ImageView) this.findViewById(R.id.image_10);
+        ImageView image_11 = (ImageView) this.findViewById(R.id.image_11);
+        ImageView image_12 = (ImageView) this.findViewById(R.id.image_12);
+        ImageView image_13 = (ImageView) this.findViewById(R.id.image_13);
 
-        this.setContentView(R.layout.how_to_play_v2);
+        image_1.setImageResource(R.drawable.image_1);
+        if(displayLanguage.equals("English")) {
+            image_2.setImageResource(R.drawable.image_2_english);
+            image_3.setImageResource(R.drawable.image_3_english);
+            image_4.setImageResource(R.drawable.image_4_english);
+            image_5.setImageResource(R.drawable.image_5_english);
+            image_6.setImageResource(R.drawable.image_6_english);
+            image_7.setImageResource(R.drawable.image_7_english);
+            image_8.setImageResource(R.drawable.image_8_english);
+            image_9.setImageResource(R.drawable.image_9_english);
+            image_10.setImageResource(R.drawable.image_10_english);
+            image_11.setImageResource(R.drawable.image_11_english);
+            image_12.setImageResource(R.drawable.image_12_english);
+            image_13.setImageResource(R.drawable.image_13_english);
+        }else{
+            image_2.setImageResource(R.drawable.image_2);
+            image_3.setImageResource(R.drawable.image_3);
+            image_4.setImageResource(R.drawable.image_4);
+            image_5.setImageResource(R.drawable.image_5);
+            image_6.setImageResource(R.drawable.image_6);
+            image_7.setImageResource(R.drawable.image_7);
+            image_8.setImageResource(R.drawable.image_8);
+            image_9.setImageResource(R.drawable.image_9);
+            image_10.setImageResource(R.drawable.image_10);
+            image_11.setImageResource(R.drawable.image_11);
+            image_12.setImageResource(R.drawable.image_12);
+            image_13.setImageResource(R.drawable.image_13);
+        }
 
         TextView first_paragraph = (TextView) this.findViewById(R.id.first_paragraph);
         first_paragraph.setText(R.string.paragraph_1);
