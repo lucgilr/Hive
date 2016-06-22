d4fc84e68eb3413e47ea3cf1194aca4b021b7466
lucgilr <luciagilroman@gmail.com>
2016-06-22 19:18:14 +0200

    [UI] new "how to play" - works also in smartphones

--- app/src/main/java/lucgilr/inf/uva/hivev2/UI/HowToPlay.java
+++ app/src/main/java/lucgilr/inf/uva/hivev2/UI/HowToPlay.java
@@ -3,47 +3,28 @@ package lucgilr.inf.uva.hivev2.UI;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.annotation.TargetApi;
-import android.app.ActionBar;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.WindowManager.LayoutParams;
-import android.widget.Toast;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import lucgilr.inf.uva.hivev2.R;
 
 /**
- * Created by Lucía Gil Román on 07/06/16.
+ * @author Lucía Gil Román
+ *
+ * Rules of the Game!
  */
 public class HowToPlay extends FragmentActivity {
 
     private String displayLanguage;
-
-    public static final String EXTRA_IMAGE = "extra_image";
-
-    //private ImagePagerAdapter mAdapter;
-    private ViewPager mPager;
-
-    // A static dataset to back the ViewPager adapter
-    public final static Integer[] imageSpanish = new Integer[] {
-            R.drawable.image_1, R.drawable.image_2, R.drawable.image_3,
-            R.drawable.image_4, R.drawable.image_5, R.drawable.image_6,
-            R.drawable.image_7, R.drawable.image_8, R.drawable.image_9,
-            R.drawable.image_10, R.drawable.image_11, R.drawable.image_12,
-            R.drawable.image_13};
+    private final int numberImages = 13;
+    private final int numberParagraphs = 14;
+    private int countImages;
+    private int countParagraphs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,96 +33,120 @@ public class HowToPlay extends FragmentActivity {
 
         this.setContentView(R.layout.how_to_play);
 
-        //mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), imageSpanish.length);
+        this.countImages = 1;
+        this.countParagraphs = 1;
 
         //Language
         displayLanguage = Locale.getDefault().getDisplayLanguage();
 
-        ImageView image_1 = (ImageView) this.findViewById(R.id.image_1);
-        ImageView image_2 = (ImageView) this.findViewById(R.id.image_2);
-        ImageView image_3 = (ImageView) this.findViewById(R.id.image_3);
-        ImageView image_4 = (ImageView) this.findViewById(R.id.image_4);
-        ImageView image_5 = (ImageView) this.findViewById(R.id.image_5);
-        ImageView image_6 = (ImageView) this.findViewById(R.id.image_6);
-        ImageView image_7 = (ImageView) this.findViewById(R.id.image_7);
-        ImageView image_8 = (ImageView) this.findViewById(R.id.image_8);
-        ImageView image_9 = (ImageView) this.findViewById(R.id.image_9);
-        ImageView image_10 = (ImageView) this.findViewById(R.id.image_10);
-        ImageView image_11 = (ImageView) this.findViewById(R.id.image_11);
-        ImageView image_12 = (ImageView) this.findViewById(R.id.image_12);
-        ImageView image_13 = (ImageView) this.findViewById(R.id.image_13);
+        //First image and paragraph
+        final ImageView image = (ImageView) this.findViewById(R.id.image_1);
+        final TextView paragraph = (TextView) this.findViewById(R.id.first_paragraph);
+        image.setImageResource(R.drawable.image_1);
+        paragraph.setText(R.string.paragraph_1);
 
-        image_1.setImageResource(R.drawable.image_1);
-        if(displayLanguage.equals("English")) {
-            image_2.setImageResource(R.drawable.image_2_english);
-            image_3.setImageResource(R.drawable.image_3_english);
-            image_4.setImageResource(R.drawable.image_4_english);
-            image_5.setImageResource(R.drawable.image_5_english);
-            image_6.setImageResource(R.drawable.image_6_english);
-            image_7.setImageResource(R.drawable.image_7_english);
-            image_8.setImageResource(R.drawable.image_8_english);
-            image_9.setImageResource(R.drawable.image_9_english);
-            image_10.setImageResource(R.drawable.image_10_english);
-            image_11.setImageResource(R.drawable.image_11_english);
-            image_12.setImageResource(R.drawable.image_12_english);
-            image_13.setImageResource(R.drawable.image_13_english);
-        }else{
-            image_2.setImageResource(R.drawable.image_2);
-            image_3.setImageResource(R.drawable.image_3);
-            image_4.setImageResource(R.drawable.image_4);
-            image_5.setImageResource(R.drawable.image_5);
-            image_6.setImageResource(R.drawable.image_6);
-            image_7.setImageResource(R.drawable.image_7);
-            image_8.setImageResource(R.drawable.image_8);
-            image_9.setImageResource(R.drawable.image_9);
-            image_10.setImageResource(R.drawable.image_10);
-            image_11.setImageResource(R.drawable.image_11);
-            image_12.setImageResource(R.drawable.image_12);
-            image_13.setImageResource(R.drawable.image_13);
-        }
+        //Setting maps
+        final Map<String,Integer> map = new HashMap<>();
+        map.put("1",R.drawable.image_1);
+        map.put("2",R.drawable.image_2);
+        map.put("3",R.drawable.image_3);
+        map.put("4",R.drawable.image_4);
+        map.put("5",R.drawable.image_5);
+        map.put("6",R.drawable.image_6);
+        map.put("7",R.drawable.image_7);
+        map.put("8",R.drawable.image_8);
+        map.put("9",R.drawable.image_9);
+        map.put("10",R.drawable.image_10);
+        map.put("11",R.drawable.image_11);
+        map.put("12", R.drawable.image_12);
+        map.put("13", R.drawable.image_13);
 
-        TextView first_paragraph = (TextView) this.findViewById(R.id.first_paragraph);
-        first_paragraph.setText(R.string.paragraph_1);
+        final Map<String,Integer> mapEnglish = new HashMap<>();
+        mapEnglish.put("1",R.drawable.image_1);
+        mapEnglish.put("2",R.drawable.image_2_english);
+        mapEnglish.put("3", R.drawable.image_3_english);
+        mapEnglish.put("4",R.drawable.image_4_english);
+        mapEnglish.put("5",R.drawable.image_5_english);
+        mapEnglish.put("6",R.drawable.image_6_english);
+        mapEnglish.put("7",R.drawable.image_7_english);
+        mapEnglish.put("8",R.drawable.image_8_english);
+        mapEnglish.put("9",R.drawable.image_9_english);
+        mapEnglish.put("10",R.drawable.image_10_english);
+        mapEnglish.put("11",R.drawable.image_11_english);
+        mapEnglish.put("12", R.drawable.image_12_english);
+        mapEnglish.put("13", R.drawable.image_13_english);
 
-        TextView second_paragraph = (TextView) this.findViewById(R.id.second_paragraph);
-        second_paragraph.setText(R.string.paragraph_2);
+        final Map<String,Integer> mapParagraphs = new HashMap<>();
+        mapParagraphs.put("1",R.string.paragraph_1);
+        mapParagraphs.put("2",R.string.paragraph_2);
+        mapParagraphs.put("3",R.string.paragraph_3);
+        mapParagraphs.put("4",R.string.paragraph_4);
+        mapParagraphs.put("5",R.string.paragraph_5);
+        mapParagraphs.put("6",R.string.paragraph_6);
+        mapParagraphs.put("7",R.string.paragraph_7);
+        mapParagraphs.put("8",R.string.paragraph_8);
+        mapParagraphs.put("9",R.string.paragraph_9);
+        mapParagraphs.put("10",R.string.paragraph_10);
+        mapParagraphs.put("11",R.string.paragraph_11);
+        mapParagraphs.put("12",R.string.paragraph_12);
+        mapParagraphs.put("13",R.string.paragraph_13);
+        mapParagraphs.put("14",R.string.paragraph_14);
 
-        TextView third_paragraph = (TextView) this.findViewById(R.id.third_paragraph);
-        third_paragraph.setText(R.string.paragraph_3);
+        paragraph.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (countImages < numberImages + 1) {
+                    nextImage();
+                    String imageMsg = String.valueOf(countImages);
+                    if (displayLanguage.equals("English")) {
+                        image.setImageResource(mapEnglish.get(imageMsg));
+                    } else {
+                        image.setImageResource(map.get(imageMsg));
+                    }
+                }
+                if (countParagraphs < numberParagraphs + 1) {
+                    nextParagraph();
+                    String paragraphMsg = String.valueOf(countParagraphs);
+                    paragraph.setText(mapParagraphs.get(paragraphMsg));
+                }
+            }
+        });
 
-        TextView fourth_paragraph = (TextView) this.findViewById(R.id.fourth_paragraph);
-        fourth_paragraph.setText(R.string.paragraph_4);
+        image.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previousImage();
+                if(countImages>0){
+                    String imageMsg = String.valueOf(countImages);
+                    if(displayLanguage.equals("English")){
+                        image.setImageResource(mapEnglish.get(imageMsg));
+                    }else {
+                        image.setImageResource(map.get(imageMsg));
+                    }
+                }
+                previousParagraph();
+                if(countParagraphs>0){
+                    String paragraphMsg = String.valueOf(countParagraphs);
+                    paragraph.setText(mapParagraphs.get(paragraphMsg));
+                }
+            }
+        });
+    }
 
-        TextView fifth_paragraph = (TextView) this.findViewById(R.id.fifth_paragraph);
-        fifth_paragraph.setText(R.string.paragraph_5);
+    private void nextImage(){
+        if(this.countImages<13) this.countImages+=1;
+    }
 
-        TextView sixth_paragraph = (TextView) this.findViewById(R.id.sixth_paragraph);
-        sixth_paragraph.setText(R.string.paragraph_6);
+    private void nextParagraph(){
+        if(this.countParagraphs<14) this.countParagraphs+=1;
+    }
 
-        TextView seventh_paragraph = (TextView) this.findViewById(R.id.seventh_paragraph);
-        seventh_paragraph.setText(R.string.paragraph_7);
+    private void previousImage(){
+        if(this.countImages>1) this.countImages-=1;
+    }
 
-        TextView eighth_paragraph = (TextView) this.findViewById(R.id.eighth_paragraph);
-        eighth_paragraph.setText(R.string.paragraph_8);
-
-        TextView ninth_paragraph = (TextView) this.findViewById(R.id.ninth_paragraph);
-        ninth_paragraph.setText(R.string.paragraph_9);
-
-        TextView tenth_paragraph = (TextView) this.findViewById(R.id.tenth_paragraph);
-        tenth_paragraph.setText(R.string.paragraph_10);
-
-        TextView eleventh_paragraph = (TextView) this.findViewById(R.id.eleventh_paragraph);
-        eleventh_paragraph.setText(R.string.paragraph_11);
-
-        TextView twelfth_paragraph = (TextView) this.findViewById(R.id.twelfth_paragraph);
-        twelfth_paragraph.setText(R.string.paragraph_12);
-
-        TextView thirtieth_paragraph = (TextView) this.findViewById(R.id.thirtieth_paragraph);
-        thirtieth_paragraph.setText(R.string.paragraph_13);
-
-        TextView fourteenth_paragraph = (TextView) this.findViewById(R.id.fourteenth_paragraph);
-        fourteenth_paragraph.setText(R.string.paragraph_14);
-
+    private void previousParagraph(){
+        if(this.countParagraphs>1) this.countParagraphs-=1;
     }
 
 }
